a4fc22d3df63bfb96b014553b217fe69444e413d
Marcos Vieira <marcosvieira482@gmail.com>
2024-06-01 13:38:03 -0300

    feat: added quiz logic

--- quiz/src/components/QuizGame.jsx
+++ quiz/src/components/QuizGame.jsx
@@ -7,14 +7,20 @@ import Button from "./Button";
 
 const QuizGame = () => {
     const [quizState, dispatch] = useContext(QuizContext);
+    const currentQuestion = quizState.questions[quizState.currentQuestion];
     return (
         <div className="bg-red-500">
             <p>Desafio {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
-            <Image src="" />
+            <Image src={`src/img/monsters/${currentQuestion.image}.png`} />
             <p>Que Monstro é esse?</p>
             <div>
-                <Button text="Continuar" />
+                <Button text={currentQuestion.options[0]} />
+                <Button text={currentQuestion.options[1]} />
+                <Button text={currentQuestion.options[2]} />
+                <Button text={currentQuestion.options[3]} />
             </div>
+            <h2>{currentQuestion.options[currentQuestion.answer - 1]}</h2>
+            <Button click={() => dispatch({ type: "CHANGE_QUESTION" })} text="Continuar" />
         </div>
     );
 }
