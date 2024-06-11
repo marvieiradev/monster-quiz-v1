6f40301a5a1554f9e57ca6636675388936f60977
Marcos Vieira <marcosvieira482@gmail.com>
2024-06-10 23:00:46 -0300

    feat: update Start page styles

--- quiz/src/components/Start.jsx
+++ quiz/src/components/Start.jsx
@@ -9,11 +9,12 @@ const Start = () => {
     const [quizState, dispatch] = useContext(QuizContext);
     return (
         <div className="app flex flex-col items-center w-[70%] justify-center h-[100vh] relative gap-8">
-            <span className="v-line absolute left-0 block"></span>
-            <span className="v-line absolute right-0 rotate-180 block"></span>
+            <span className="h-line absolute bottom-4 block"></span>
+            <span className="h-line absolute top-4 rotate-180 block"></span>
+
             <h2 className="text-2xl font-bold">Monster Quiz</h2>
             <p>Clique no botão para iniciar</p>
-            <div className="w-[500px] flex justify-center">
+            <div className="w-[400px] flex justify-center">
                 <img src={Logo} alt="logo" width="90%" />
             </div>
             <Button click={() => dispatch({ type: "CHANGE_STAGE" })} text="INICIAR" />
