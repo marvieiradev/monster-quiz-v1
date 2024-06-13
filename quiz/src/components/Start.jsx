358033d406a1a69b522cfb67bb9feb0612f39fea
Marcos Vieira <marcosvieira482@gmail.com>
2024-06-13 19:33:19 -0300

    feat: added partial responsiveness on the Start page

--- quiz/src/components/Start.jsx
+++ quiz/src/components/Start.jsx
@@ -1,21 +1,21 @@
 import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 
-import Logo from "../img/logo.webp";
+import Logo from "../img/logo.png";
 import Button from "./Button";
 
 const Start = () => {
     const [quizState, dispatch] = useContext(QuizContext);
     return (
-        <div className="app start flex flex-col items-center w-[70%] justify-center h-[100vh] relative gap-8">
+        <div className="app start flex flex-col items-center w-full justify-center h-[100vh] relative gap-8">
             <span className="h-line absolute bottom-0 block"></span>
             <span className="h-line absolute top-0 rotate-180 block"></span>
-            <div className="w-[400px] flex justify-center">
-                <img src={Logo} alt="logo" width="100%" />
+            <div className="w-[300px] flex justify-center -rotate-[11deg]">
+                <img src={Logo} alt="logo" width="90%" />
             </div>
-            <h2 className="text-2xl leading-8 font-bold mx-14 text-center">Teste seus conhecimentos no Monster Hunter! Você conhece todos os monstros da franquia? Responda o nome do monstro de acordo com o sua imagem</h2>
+            <h2 className="text-lg leading-6 font-bold mx-14 text-center">Teste seus conhecimentos no Monster Hunter! Você conhece todos os monstros da franquia? Responda o nome do monstro de acordo com o sua imagem</h2>
             <Button click={() => dispatch({ type: "CHANGE_STAGE" })} text="INICIAR" />
-            <p className="font-bold">[Atenção: Esse app foi feito de fã para fã. Não é um produto oficial da franquia]</p>
+            <p className="font-bold text-sm text-center">[Atenção: Esse app foi feito de fã para fã. Não é um produto oficial da franquia]</p>
         </div>
     );
 }
