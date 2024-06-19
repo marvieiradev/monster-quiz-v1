ef55d6f9060f6e1a75a9d02238e06153f9a45ef8
Marcos Vieira <marcosvieira482@gmail.com>
2024-06-19 20:23:04 -0300

    feat: add progress bar

--- quiz/src/components/QuizGame.jsx
+++ quiz/src/components/QuizGame.jsx
@@ -17,6 +17,8 @@ const QuizGame = () => {
             payload: { answer: currentQuestion.options[currentQuestion.answer - 1], option },
         });
     }
+
+    var perc = quizState.currentQuestion + 1;
     return (
         <>
             <div className="app flex flex-col items-center w-full max-w-[500px] justify-center h-[100vh] relative">
@@ -24,7 +26,10 @@ const QuizGame = () => {
                     <span className="v-line absolute left-0 block"></span>
                     <span className="v-line absolute right-0 rotate-180 block"></span>
                     <img src={Logo} alt="logo" className="w-[70%]" />
-                    <p className="text-xl lg:text-2xl">Desafio {quizState.currentQuestion + 1} de {quizState.maxQuestions}</p>
+                    <p className="text-xl lg:text-2xl mb-[-10px]">Desafio {quizState.currentQuestion + 1} de {quizState.maxQuestions}</p>
+                    <div className="w-[80%]">
+                        <img src={`./bar/p${Math.ceil(perc / 2)}.svg`} alt="" className="mt-[-5px] pointer-events-none" />
+                    </div>
                     <div className="w-[150px] flex items-center justify-center relative p-[5px]  bg-white rounded-xl md:w-[200px]">
                         <img src={`${baseURL}small/${currentQuestion.id}.webp`} alt="monster" />
                         <img src={Frame} className="absolute" />
