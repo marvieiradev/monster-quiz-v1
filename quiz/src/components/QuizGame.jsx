65822354534318a746a60d2d71d9b5424f941b30
Marcos Vieira <marcosvieira482@gmail.com>
2024-06-25 20:01:30 -0300

    feat: remove correct answer show

--- quiz/src/components/QuizGame.jsx
+++ quiz/src/components/QuizGame.jsx
@@ -37,7 +37,7 @@ const QuizGame = () => {
                     </div>
                     <p className="text-xl lg:text-2xl">Que Monstro é esse?</p>
 
-                    <p className="">{currentQuestion.options[currentQuestion.answer - 1]}</p>
+                    {/* <p className="">{currentQuestion.options[currentQuestion.answer - 1]}</p>*/}
 
                     <div className="options w-[80%] flex flex-col gap-4 text-lg">
                         {currentQuestion.options.map((option) => (
