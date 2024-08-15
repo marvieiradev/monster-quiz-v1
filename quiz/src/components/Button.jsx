636cb4d2463d78fd42e844103543bac8de60e53f
Marcos Vieira <marcosvieira482@gmail.com>
2024-08-15 20:17:25 -0300

    feat: update button style

--- quiz/src/components/Button.jsx
+++ quiz/src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = (props) => {
     return (
         <>
-            < div onClick={props.click} className="btn cursor-pointer justify-center">
+            < div onClick={props.click} className="btn cursor-pointer justify-center hover:scale-105">
                 <span className="text-white text-md text-center md:text-xl">{props.text}</span>
             </div>
         </>
